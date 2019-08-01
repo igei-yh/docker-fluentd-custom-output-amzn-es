928728d1a6a56708706263a0a615bacdb801fa62
yohei.igei <yohei.igei@neo-lab.co.jp>
2019-08-02 00:09:29 +0900

    add fluentd.conf, docker custom entrypoint.sh.

---
+++ custom-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+if [ -n ${AMZN_ES_ENDPOINT} ]; then
+        sed -i -e 's~AMZN_ES_ENDPOINT~'${AMZN_ES_ENDPOINT}'~g' /fluentd/etc/fluentd.conf
+fi
+if [ -n ${AMZN_ES_REGION} ]; then
+        sed -i -e 's~AMZN_ES_REGION~'${AMZN_ES_REGION}'~g' /fluentd/etc/fluentd.conf
+fi
+
+# quote original entrypoint.sh
+DEFAULT=/etc/default/fluentd
+
+if [ -r $DEFAULT ]; then
+    set -o allexport
+    . $DEFAULT
+    set +o allexport
+fi
+
+# If the user has supplied only arguments append them to `fluentd` command
+if [ "${1#-}" != "$1" ]; then
+    set -- fluentd "$@"
+fi
+
+# If user does not supply config file or plugins, use the default
+if [ "$1" = "fluentd" ]; then
+    if ! echo $@ | grep ' \-c' ; then
+       set -- "$@" -c /fluentd/etc/${FLUENTD_CONF}
+    fi
+
+    if ! echo $@ | grep ' \-p' ; then
+       set -- "$@" -p /fluentd/plugins
+    fi
+fi
+
+exec "$@"
